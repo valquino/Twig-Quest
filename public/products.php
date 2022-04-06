d62b36e7565d6ef3dc6d317f3d903bedc2500de5
titiphenom-wcs <valentino-quinola_student2022@wilder.school>
2022-04-06 17:07:28 +0200

    add products page

---
+++ public/products.php
@@ -0,0 +1,8 @@
+<?php
+// Get a $twig object from this file.
+require_once __DIR__ . '/../config/twig.php';
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+echo $twig->render('products.html.twig', ['products' => $products]);
+
+?>
