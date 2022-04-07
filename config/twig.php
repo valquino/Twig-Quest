a8e4203ff85e17a40cb141b5bf7992b5b8472466
titiphenom-wcs <valentino-quinola_student2022@wilder.school>
2022-04-07 11:46:18 +0200

    add a footer element using global variable with Twig

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+//On ajoute la globale contactEmail à l'initialisation de l'objet Twig
+$contactEmail = "twig@wildcodeschool.com";
+$twig->addGlobal('contactEmail', $contactEmail);
--- public/details.php
+++ public/details.php
@@ -1,6 +1,5 @@
 <?php
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
-$name = 'Wilder';
-echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('details.html.twig');
 ?>
